7227e2d51cfe73b9e67ddf251bc65a1f2b5a9c0c
mck0324 <dch3030@naver.com>
2024-02-15 22:09:20 +0900

    카페라뗴를 예로 멀티쓰레드를 이용하여 만드는 시간 알아보기

--- src/main/kotlin/sample/coffee/EmployeeCoffee.kt
+++ src/main/kotlin/sample/coffee/EmployeeCoffee.kt
@@ -1,25 +1,43 @@
 package sample.coffee
 
 import mu.KotlinLogging
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 import kotlin.system.measureTimeMillis
 
 private val logger = KotlinLogging.logger {  }
 
 
+//직원 3명 고용
+private val employees = Executors.newFixedThreadPool(3)
 fun main() {
     measureTimeMillis {
+//        makeCoffee()
         repeat(2) {
             makeCoffee()
         }
+        employees.shutdown()
+        employees.awaitTermination(100, TimeUnit.SECONDS)
     }.let { logger.debug { ">>커피 만드는데 걸리는 시간: $it ms" } }
 }
 
 private fun makeCoffee() {
-    grindCoffee()
-    brewCoffee()
-    boilMilk()
-    formMilk()
-    mixCoffeeAndMilk()
+    val taskA = employees.submit {
+        grindCoffee()
+        brewCoffee()
+    }
+
+    val taskB = employees.submit {
+        boilMilk()
+        formMilk()
+    }
+    employees.submit {
+        while ( !taskA.isDone || !taskB.isDone ) {
+            Thread.sleep(10)
+        }
+        mixCoffeeAndMilk()
+
+    }
 
 }
 private fun grindCoffee() {
@@ -52,6 +70,6 @@ private fun mixCoffeeAndMilk() {
     logger.debug { ">> 카페라떼 완성" }
 }
 
-//Block방식
-//순차적으로 실행되므로 커피 한잔 만드는데 걸리는 시간:5035 ms
-//순차적으로 실행되므로 커피 두잔 만드는데 걸리는 시간:10071 ms
+//멀티쓰레드로 만들땐
+//커피 한잔 만드는데 걸리는 시간: 3042 ms
+// 커피 두잔 만드는데 걸리는 시간: 5046 ms
